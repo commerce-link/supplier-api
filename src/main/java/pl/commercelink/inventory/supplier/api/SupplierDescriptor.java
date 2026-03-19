15b830926cc7955cbfbb433f6640f94bb48df0bf
Marcin Dziedzic <marcin@madmangaming.pl>
2026-03-19 10:59:11 +0100

    update descriptor

--- src/main/java/pl/commercelink/inventory/supplier/api/SupplierDescriptor.java
+++ src/main/java/pl/commercelink/inventory/supplier/api/SupplierDescriptor.java
@@ -5,7 +5,7 @@ import pl.commercelink.inventory.supplier.api.support.ResourceDownloadException;
 import java.util.Optional;
 
 public interface SupplierDescriptor {
-    Optional<FeedData> download() throws ResourceDownloadException;
+    Optional<FeedData> download(String secret) throws ResourceDownloadException;
 
     FeedFormat feedFormat();
     SupplierInfo supplierInfo();
