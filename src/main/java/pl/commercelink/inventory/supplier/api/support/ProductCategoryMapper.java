dd8e1c369f36801f3934fa4aab3aa1f4c998d4b8
SzymonReddigPC <szymon.reddig@pragmaticcoders.com>
2026-07-14 17:55:17 +0200

    Drop the ProductCategory enum from the supplier contract

--- src/main/java/pl/commercelink/inventory/supplier/api/support/ProductCategoryMapper.java
+++ src/main/java/pl/commercelink/inventory/supplier/api/support/ProductCategoryMapper.java
@@ -1,6 +1,6 @@
 package pl.commercelink.inventory.supplier.api.support;
 
-import pl.commercelink.taxonomy.ProductCategory;
+import pl.commercelink.inventory.supplier.api.Taxonomy;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,21 +13,21 @@ public class ProductCategoryMapper {
         this.mappings = mappings;
     }
 
-    public ProductCategory from(String group, String subgroup) {
+    public String from(String group, String subgroup) {
         for (Mapping mapping : mappings) {
             if (mapping.matches(group, subgroup)) {
                 return mapping.productCategory;
             }
         }
-        return ProductCategory.Other;
+        return Taxonomy.OTHER;
     }
 
     public static class Mapping {
-        private final ProductCategory productCategory;
+        private final String productCategory;
         private String group;
         private List<String> subgroups;
 
-        public Mapping(ProductCategory productCategory, String group, String[] subgroups) {
+        public Mapping(String productCategory, String group, String[] subgroups) {
             this.productCategory = productCategory;
 
             this.group = group;
--- src/test/java/pl/commercelink/inventory/supplier/api/TaxonomyTest.java
+++ src/test/java/pl/commercelink/inventory/supplier/api/TaxonomyTest.java
@@ -1,11 +1,9 @@
 package pl.commercelink.inventory.supplier.api;
 
 import org.junit.jupiter.api.Test;
-import pl.commercelink.taxonomy.ProductCategory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TaxonomyTest {
@@ -20,44 +18,9 @@ class TaxonomyTest {
     }
 
     @Test
-    void enumConstructorBridgesToCategoryName() {
+    void constructorUnifiesEanAndMfn() {
         // given / when
-        Taxonomy taxonomy = new Taxonomy("5901234123457", "MFN-1", "BrandX", "Laptop Pro",
-                ProductCategory.Laptops, 10, 1200, 1500);
-
-        // then
-        assertEquals("Laptops", taxonomy.category());
-        assertEquals(1200, taxonomy.netWeightInGrams());
-        assertEquals(1500, taxonomy.grossWeightInGrams());
-    }
-
-    @Test
-    void shortEnumConstructorBridgesToCategoryName() {
-        // given / when
-        Taxonomy taxonomy = new Taxonomy("5901234123457", "MFN-1", "BrandX", "Laptop Pro",
-                ProductCategory.Laptops, 10);
-
-        // then
-        assertEquals("Laptops", taxonomy.category());
-        assertNull(taxonomy.netWeightInGrams());
-        assertNull(taxonomy.grossWeightInGrams());
-    }
-
-    @Test
-    void nullEnumCategoryBridgesToNullString() {
-        // given / when
-        Taxonomy taxonomy = new Taxonomy("5901234123457", "MFN-1", "BrandX", "Laptop Pro",
-                (ProductCategory) null, 10);
-
-        // then
-        assertNull(taxonomy.category());
-    }
-
-    @Test
-    void enumConstructorUnifiesEanAndMfn() {
-        // given / when
-        Taxonomy taxonomy = new Taxonomy("0590123412345", "mfn 1", "BrandX", "Laptop Pro",
-                ProductCategory.Laptops, 10);
+        Taxonomy taxonomy = new Taxonomy("0590123412345", "mfn 1", "BrandX", "Laptop Pro", "Laptops", 10, null, null);
 
         // then
         assertEquals("590123412345", taxonomy.ean());
